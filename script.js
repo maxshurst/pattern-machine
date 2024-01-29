2d03206f4e46ae7bd89cb354dea6d40aeb5fe392
Linden <50274080+liulinden@users.noreply.github.com>
2024-01-29 12:00:07 -0800

    progress

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const number1box = document.getElementById('number1input');
 
 const number2box = document.getElementById('number2input');
+const box = document.getElementById('box');
 
 const colorbox = document.getElementById('colorinput');
+var boxes = []
 
 
 function addClone(){
 
-    var cloned = document.getElementById('box').cloneNode(true); //clone the element
-    document.body.appendChild(cloned); //add the cloned element to the page
-
+    const color = colorbox.value;
+    const rows = number1box.value;
+    const columns = number2box.value;
+    for (let i=0;i<rows; i++){
+        for (let i=0;i<rows; i++){
+            var cloned = document.getElementById('box').cloneNode(true); //clone the element
+            cloned.style.left=i*150;
+            cloned.style.right=i*150;
+            document.body.appendChild(cloned); //add the cloned element to the page
+        }
+    }
+    
 }
 
 function submit(){
 
-const color = colorbox.value;
-const rows = number1box.value;
-const columns = number2box.value;
+    const color = colorbox.value;
+    const rows = number1box.value;
+    const columns = number2box.value;
 
-box.style.backgroundColor = color; // WORK ON THIS
+    box.style.backgroundColor = color; // WORK ON THIS
 
 
 
