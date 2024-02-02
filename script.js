492af9f746672610cf337d3677f5a299a5394d83
Linden <50274080+liulinden@users.noreply.github.com>
2024-02-02 12:19:22 -0800

    finished bare bone project

--- script.js
+++ script.js
@@ -6,30 +6,33 @@ const box = document.getElementById('box');
 const colorbox = document.getElementById('colorinput');
 var boxes = 0;
 
+function deleteClones(){
+    const toDelete=document.getElementsByClassName('clone');
+    console.log(toDelete);
+    console.log(toDelete.length)
+    for (let i=toDelete.length-1;i>=0;i--){
+        toDelete[i].remove();
+        console.log(toDelete[i])
+    }
+}
 
 function submit(){
     
-    const toDelete=document.getElementsByClassName('clone');
-    for (i=0;i<toDelete.length;i++){
-        toDelete[i].remove();
-    }
-    console.log(toDelete);
-    console.log(toDelete.length);
-
+    deleteClones();
     boxes=0;
-    const color = colorbox.value;
     const rows = number1box.value;
     const columns = number2box.value;
 
-    box.style.backgroundColor = color;
+    box.style.backgroundColor = colorbox.value;
     
     for (let i=0;i<rows; i++){
         for (let j=0;j<columns; j++){
             
-            const cloned = document.createElement("div")
+            let cloned = document.createElement("div")
             cloned.style.left=(j*120+100).toString()+"px";
             cloned.style.top=(i*120+100).toString()+"px";
             cloned.classList.add('clone');
+            cloned.addEventListener("click", changeColor);
             cloned.style.backgroundColor = color;
             cloned.id = `${i}&${j}`;
             document.body.appendChild(cloned); //add the cloned element to the page
@@ -37,4 +40,8 @@ function submit(){
     }
     boxes=rows*columns;
     
+}
+
+function changeColor(event){
+    event.target.style.backgroundColor=colorbox.value;
 }
